9fd8456c85793ef29dcb9c75081b3ca656a1e3aa
embeddDev <danielbergmann1@gmail.com>
2014-10-13 18:38:05 +0000

    Added files

---
+++ Project3.R
@@ -0,0 +1,8 @@
+#TASK 1 - project 3
+
+############################################################################################
+#course:  Time Series Analysis, T-862-TIMA
+#Date:    10/09/2014
+#Students: Daniel Bergmann Sigtryggsson, Lilja Bjorg Gudmundsdottir, Jon Vilberg Georgsson 
+#
+############################################################################################
