939b98fd98b603888cd8d64b36e13eeed792117a
Daniel Bergmann <danielbergmann1@gmail.com>
2014-10-14 19:55:47 +0000

    fyrsta atlaga ad daemi 1

--- Project3.R
+++ Project3.R
@@ -2,7 +2,33 @@
 
 ############################################################################################
 #course:  Time Series Analysis, T-862-TIMA
-#Date:    10/09/2014
+#Date:    //2014
 #Students: Daniel Bergmann Sigtryggsson, Lilja Bjorg Gudmundsdottir, Jon Vilberg Georgsson 
 #
 ############################################################################################
+
+
+# Brain storming excersice (10%)
+# Identify causal variables and hypothize an econometric structural equation for the following variables;
+# . Stock price index, for your home country,
+
+#OMX =  beta0 -ISL_GENGI * beta1 + epsilon
+#Heakandi kronu gengi laekkar hlutabrefavisitoluna, thi staerstu felogin gera upp i erlendum gjaldeyri.
+#haegt ad nalgast upplysingar t.d. hja gamma.is
+
+# . Scandinavian electricity market(Nordpool) or the electrical market of your home country(for foreign
+#                  students only, as there is no spot market in Iceland),
+
+#NORDPOOL = beta0 - útihitastig*beta1 + epsilon
+
+# haerra utihitastig laekkar eftirspurn eftir rafmagni, vedurupplysingar eru audvelt ad nalgast
+
+# . Inflation.
+#INFLATION = beta0 + styrivextir * beta1 + VSK*beta2 + epsilon
+# haerri styrivextri og haekkandi VSK hækkar verdbolgu, gögn til hja t.d. hagstofu
+
+# Comment on the sign and possible magnitudes and discuss whether all of the required data are available.
+# Housing Economics (40%)
+
+
+
